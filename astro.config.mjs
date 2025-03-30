1dcb77a99016ff6ff8196aeced960586f603db55
marvin-j97 <marvin.janke.97@gmail.com>
2025-03-30 18:04:38 +0200

    2.8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,34 @@
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import { defineConfig } from "astro/config";
 import unocss from "unocss/astro";
-import config from "./src/config";
 
-import mdx from "@astrojs/mdx";
+import config from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     unocss({
-      injectReset: true
+      injectReset: true,
     }),
     sitemap(),
     solidJs(),
-    mdx()
+    mdx(),
   ],
   site: config.site.url,
   base: config.site.baseUrl,
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: "hover"
+    defaultStrategy: "hover",
   },
   markdown: {
     shikiConfig: {
-      themes: config.post.code.theme
-    }
+      themes: config.post.code.theme,
+    },
   },
   redirects: {
+    "/post/announcing-fjall-2": "/post/fjall-2",
     "/post/announcing-fjall-22": "/post/fjall-2-2",
-  }
+  },
 });
